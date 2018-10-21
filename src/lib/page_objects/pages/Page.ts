f0f94a72f839ef56df8bb8536bde4dd58270bad9
Florian Hilbinger <florian.hilbinger@hotmail.com>
2018-10-21 12:53:15 +0200

    added isOpened to page

--- src/lib/page_objects/pages/Page.ts
+++ src/lib/page_objects/pages/Page.ts
@@ -2,14 +2,31 @@ import { PageElementStore } from '../stores'
 
 export interface IPageArgs<Store extends PageElementStore> {
   elementStore: Store,
+  timeout?: number
 }
 
-export class Page<Store extends PageElementStore> {
+export abstract class Page<Store extends PageElementStore> {
   protected elementStore: Store
+  protected timeout: number
 
-  // if page needs to be initialized with specific values,
-  // pass them in initObj
   constructor(args: IPageArgs<Store>) {
     this.elementStore = args.elementStore
+
+    if ( args.timeout ) {
+      this.timeout = args.timeout
+    } else {
+      this.timeout = JSON.parse(process.env.WORKFLO_CONFIG).timeouts.default
+    }
+  }
+
+  abstract isOpened()
+
+  eventuallyIsOpened( timeout: number = this.timeout ) {
+    try {
+      browser.waitUntil( () => this.isOpened(), timeout )
+      return true
+    } catch( error ) {
+      return false
+    }
   }
 }
