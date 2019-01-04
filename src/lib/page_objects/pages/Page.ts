7c979af9a8567dd852fe660ff74245302c7064f7
Florian Hilbinger <florian.hilbinger@hotmail.com>
2019-01-04 15:46:30 +0100

    added documentation for Page

--- src/lib/page_objects/pages/Page.ts
+++ src/lib/page_objects/pages/Page.ts
@@ -6,13 +6,13 @@ import { DEFAULT_TIMEOUT, DEFAULT_INTERVAL, stores } from '../'
  */
 export interface IPageOpts<Store extends PageElementStore> extends Workflo.ITimeoutInterval {
   /**
-   * An instance of PageElementStore which can be used to retrieve/create PageNodes via Page
+   * an instance of PageElementStore which can be used to retrieve/create PageNodes via Page
    */
   store: Store
 }
 
 /**
- * This class is supposed to be used as base class for all Pages.
+ * This class serves as the base class for all Pages.
  *
  * @template Store type of the PageElementStore instance which can be used to retrieve/create PageNodes via Page
  * @template IsOpenOpts type of the opts parameter passed to the functions `isOpen`, `wait.isOpen` and
@@ -32,6 +32,11 @@ export abstract class Page<
   wait: PageWait<Store, this, IsOpenOpts, IsClosedOpts>
   eventually: PageEventually<Store, this, IsOpenOpts, IsClosedOpts>
 
+  /**
+   * Page serves as the base class for all Pages.
+   *
+   * @param opts the options required to create an instance of Page
+   */
   constructor(opts: IPageOpts<Store>) {
     this._store = opts.store
 
@@ -42,22 +47,50 @@ export abstract class Page<
     this.eventually = new PageEventually(this)
   }
 
+  /**
+   * Returns an instance of PageElementStore which can be used to retrieve/create PageNodes via Page
+   */
   getStore() {
     return this._store
   }
 
+  /**
+   * Returns the default timeout in milliseconds used by Page for its `wait` and `eventually` functions.
+   */
   getTimeout() {
     return this._timeout
   }
 
+  /**
+   * Returns the default interval in milliseconds used by Page for its `wait` and `eventually` functions.
+   */
   getInterval() {
     return this._interval
   }
 
+  /**
+   * Checks if the Page is currently open.
+   *
+   * @param opts options needed to determine if the Page is currently open
+   */
   abstract isOpen(opts?: IsOpenOpts): boolean
+
+  /**
+   * Checks if the Page is currently closed.
+   *
+   * @param opts options needed to determine if the Page is currently closed
+   */
   abstract isClosed(opts?: IsClosedOpts): boolean
 }
 
+/**
+ * This class defines all `wait` functions of Page.
+ *
+ * @template Store type of the PageElementStore instance which can be used to retrieve/create PageNodes via Page
+ * @template PageType type of the Page for which PageWait defines all `wait` functions
+ * @template IsOpenOpts type of the opts parameter passed to the functions `isOpen`
+ * @template IsClosedOpts type of the opts parameter passed to the functions `isClosed`
+ */
 class PageWait<
   Store extends PageElementStore,
   PageType extends Page<Store, IsOpenOpts, IsClosedOpts>,
@@ -67,6 +100,11 @@ class PageWait<
 
   protected _page: PageType
 
+  /**
+   * PageWait defines all `wait` functions of Page.
+   *
+   * @param page the Page for which PageWait defines all `wait` functions
+   */
   constructor(page: PageType) {
     this._page = page;
   }
@@ -106,15 +144,37 @@ class PageWait<
     return this._page
   }
 
+  /**
+   * Waits for Page to become open and throws an error of the Page does not become open within a specific timeout.
+   *
+   * @param opts includes the options defined in IsOpenOpts, the timeout within which the Page must become open and
+   * the interval used to check for this condition.
+   * @returns an instance of Page
+   */
   isOpen(opts: IsOpenOpts & Workflo.ITimeoutInterval = Object.create(null)) {
     return this._wait(() => this._page.isOpen(opts), " to be open", opts)
   }
 
+  /**
+   * Waits for Page to become closed and throws an error of the Page does not become closed within a specific timeout.
+   *
+   * @param opts includes the options defined in IsClosedOpts, the timeout within which the Page must become closed and
+   * the interval used to check for this condition.
+   * @returns an instance of Page
+   */
   isClosed(opts: IsClosedOpts & Workflo.ITimeoutInterval = Object.create(null)) {
     return this._wait(() => this._page.isClosed(opts), " to be closed", opts)
   }
 }
 
+/**
+ * This class defines all `eventually` functions of Page.
+ *
+ * @template Store type of the PageElementStore instance which can be used to retrieve/create PageNodes via Page
+ * @template PageType type of the Page for which PageEventually defines all `eventually` functions
+ * @template IsOpenOpts type of the opts parameter passed to the function `isOpen`
+ * @template IsClosedOpts type of the opts parameter passed to the functions `isClosed`
+ */
 class PageEventually<
   Store extends PageElementStore,
   PageType extends Page<Store, IsOpenOpts, IsClosedOpts>,
@@ -124,6 +184,11 @@ class PageEventually<
 
   protected _page: PageType
 
+  /**
+   * PageEventually defines all `eventually` functions of Page.
+   *
+   * @param page the Page for which PageEventually defines all `eventually` functions
+   */
   constructor(page: PageType) {
     this._page = page;
   }
@@ -157,10 +222,22 @@ class PageEventually<
     }
   }
 
+  /**
+   * Checks if Page eventually becomes open within a specific timeout.
+   *
+   * @param opts includes the options defined in IsOpenOpts, the timeout within which the Page must become open and
+   * the interval used to check for this condition.
+   */
   isOpen(opts: IsOpenOpts & Workflo.ITimeoutInterval = Object.create(null)) {
     return this._eventually(() => this._page.isOpen(opts), opts)
   }
 
+  /**
+   * Checks if Page eventually becomes closed within a specific timeout.
+   *
+   * @param opts includes the options defined in IsClosedOpts, the timeout within which the Page must become closed and
+   * the interval used to check for this condition.
+   */
   isClosed(opts: IsClosedOpts & Workflo.ITimeoutInterval = Object.create(null)) {
     return this._eventually(() => this._page.isClosed(opts), opts)
   }
